b814550d2234e171d35fbec1d1fc23573ade5ec9
root <root@ip-172-31-47-53.ap-south-1.compute.internal>
2023-11-15 12:50:24 +0000

    updated app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return '<b>Hello,</b><br><b>this python application runs from Amazon ECS with the help of Jenkins CI/CD Pipeline!!!</b>'
+    return '<b>Hello,</b><br><b>Project 6: Setting up a Continuous Delivery Pipeline with Git, Jenkins, Docker, and AWS ECS!!!</b>'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
---
+++ jenkins.sh
@@ -0,0 +1,16 @@
+sudo apt-get update
+sudo apt-get install -y openjdk-11-jre
+sudo apt-get update
+
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install jenkins -y 
+
+sudo apt-get install -y docker.io
+sudo apt-get install -y awscli 
