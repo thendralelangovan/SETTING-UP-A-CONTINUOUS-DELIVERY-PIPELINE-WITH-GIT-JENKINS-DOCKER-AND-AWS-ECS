0065d69d816225ae855f52ec40bfc54b67c4aa09
Ravivarman J <jravivarman16@gmail.com>
2023-11-16 15:17:02 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,7 +1,11 @@
+#updating the os:
 sudo apt-get update
+
+#installing java:
 sudo apt-get install -y openjdk-11-jre
 sudo apt-get update
 
+#adding jenkins repositories files to the os:
 sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
   https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
 
@@ -9,8 +13,13 @@ echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
 
+
+#updating the os & installing jenkins:
 sudo apt-get update
 sudo apt-get install jenkins -y 
 
+#installing docker:
 sudo apt-get install -y docker.io
+
+#installing aws cli for configuring purpose:
 sudo apt-get install -y awscli 
