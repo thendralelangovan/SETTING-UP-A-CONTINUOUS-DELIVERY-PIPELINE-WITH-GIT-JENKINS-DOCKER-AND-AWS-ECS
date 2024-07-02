5044ccbbefd18e18fbcbc8de21e674bfb5ae69ab
root <root@ip-172-31-10-63.ap-south-1.compute.internal>
2024-07-02 10:46:47 +0000

    changes on app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return '<b>Hello,</b><br><b>Project 6: Setting up a Continuous Delivery Pipeline with Git, Jenkins, Docker, and AWS ECS!!!</b>'
+    return '<b>Hello,</b><br><b>Project 6: Setting up a Continuous Delivery Pipeline with Git, Jenkins, Docker, and AWS ECS!!!!</b>'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
